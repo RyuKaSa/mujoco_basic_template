c44b2b52ea20979e2b857ee63c4b55b54253eb4a
RyuKaSa <william.bogdanovic@edu.esiee.fr>
2026-01-04 23:32:31 +0100

    init drone folder, and xml

---
+++ drone/preview.py
@@ -0,0 +1,16 @@
+import mujoco
+import mujoco.viewer
+   
+while True:
+    model = mujoco.MjModel.from_xml_path("model.xml")
+    data = mujoco.MjData(model)
+    with mujoco.viewer.launch_passive(model, data) as v:
+
+        # show joints
+        # v.opt.flags[mujoco.mjtVisFlag.mjVIS_JOINT] = True
+        # enable transparency rendering
+        v.opt.flags[mujoco.mjtVisFlag.mjVIS_TRANSPARENT] = True
+        while v.is_running():
+            mujoco.mj_step(model, data)
+            v.sync()
+    # close viewer, loop reloads the file
